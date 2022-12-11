7f678d1f28f7b9dd3738e564d6f837b89d9b75dd
Paulo Truta <pinheirotruta5@gmail.com>
2022-12-11 13:39:18 +0100

    Removed unneded f string

--- lambda/utils.py
+++ lambda/utils.py
@@ -6,7 +6,7 @@ CONFIG_FILE = "config.json"
 
 def load_config() -> dict:
     if not exists(CONFIG_FILE):
-        raise ValueError(f"Config file does not exist")
+        raise ValueError("Config file does not exist")
 
     with open(CONFIG_FILE) as f:
         return json.load(f)
